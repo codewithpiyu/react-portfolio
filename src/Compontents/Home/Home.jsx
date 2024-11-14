3e62fd134191f5e9963bec41b5be399131b7e051
codewithpiyu <herballife.priyankanaik@gmail.com>
2024-11-14 15:33:43 +0530

    update: home section discription

--- src/Compontents/Home/Home.jsx
+++ src/Compontents/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className='md:w-2/4 md:pt-10'>
         <h1 className='text-3xl md:text-6xl font-bold flex leading-normal tracking-tigther'>
           <TextChange/></h1>
-        <p className='text-sm md:text-2xl tracking-tight'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis at eveniet non temporibus corrupti facilis officiis, dolor fugit harum nisi ut nesciunt minima saepe architecto consectetur voluptas reprehenderit accusantium et.</p>
+        <p className='text-sm md:text-2xl tracking-tight mt-3'>I’m a passionate Front-End Developer specializing in building responsive, interactive, and user-friendly web interfaces. With expertise in modern frameworks like React and a strong foundation in HTML, CSS, and JavaScript, I love turning creative ideas into functional web experiences.</p>
         <button className="mt-5 md:md-10 text-white py02 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
           Contact Me
         </button>
